fdc87b7e091cb206ce8f8731aae52cad46b413a8
Muhammad Akif <akifkhan6064@gmail.com>
2026-04-29 10:37:34 +0500

    Sign Up Screen Completed

--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_demo/screen/dashboard_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -14,6 +15,10 @@ class _SignupScreenState extends State<SignupScreen> {
   var emailC = TextEditingController();
   var passwordC = TextEditingController();
   var confirmPasswordC = TextEditingController();
+
+  var _obscurePassword  = true;
+
+  String? selectedGender = 'Male'; // 'Male, 'Female', 'Other'
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +38,39 @@ class _SignupScreenState extends State<SignupScreen> {
             ),),
 
           SizedBox(height: 25,),
+
+          Text('Gender'),
+          RadioGroup<String>(
+            groupValue: selectedGender,
+            onChanged: (String? newValue) {
+              setState(() {
+                selectedGender = newValue;
+              });
+            },
+            child: Row(
+              mainAxisAlignment: .spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Radio(value: 'Male'),
+                    Text('Male'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio(value: 'Female'),
+                    Text('Female'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio(value: 'Other'),
+                    Text('Other'),
+                  ],
+                ),
+              ],
+            ),
+          ),
 
           TextField(
             controller: phoneC,
@@ -55,11 +93,19 @@ class _SignupScreenState extends State<SignupScreen> {
           SizedBox(height: 25,),
 
           TextField(
+            maxLength: 8,
             controller: passwordC,
             keyboardType: TextInputType.text,
+            obscureText: _obscurePassword,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: "Password",
+                suffixIcon: IconButton(onPressed: (){
+                  setState(() {
+                    _obscurePassword = !_obscurePassword;
+                  });
+
+                }, icon: Icon(_obscurePassword? Icons.visibility : Icons.visibility_off))
             ),),
 
           SizedBox(height: 25,),
@@ -67,14 +113,61 @@ class _SignupScreenState extends State<SignupScreen> {
           TextField(
             controller: confirmPasswordC,
             keyboardType: TextInputType.text,
+            obscureText: _obscurePassword,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: "Confirm Password",
+              suffixIcon: IconButton(onPressed: (){
+                setState(() {
+                  _obscurePassword =! _obscurePassword;
+                });
+
+              }, icon: Icon(_obscurePassword? Icons.visibility: Icons.visibility_off) )
             ),),
 
           SizedBox(height: 25,),
 
           ElevatedButton(onPressed: (){
+            String name = nameC.text.trim();
+            String phone = phoneC.text.trim();
+            String email = emailC.text.trim();
+            String password = passwordC.text.trim();
+            String confirmPassword = confirmPasswordC.text.trim();
+
+            if(name.isEmpty){
+              Fluttertoast.showToast(msg: "please provide name");
+              return;
+            }
+
+            if(phone.isEmpty){
+              Fluttertoast.showToast(msg: "please provide phone");
+              return;
+            }
+
+            if(email.isEmpty){
+              Fluttertoast.showToast(msg: "please provide email");
+              return;
+            }
+
+            if(password.isEmpty){
+              Fluttertoast.showToast(msg: "please provide password");
+              return;
+            }
+
+            if(confirmPassword.isEmpty){
+              Fluttertoast.showToast(msg: "please provide confirm password");
+              return;
+            }
+
+            if(password!=confirmPassword){
+              Fluttertoast.showToast(msg: "confirm password are not matchable");
+              return;
+            }
+            if(password.length< 8){
+              Fluttertoast.showToast(msg: "Password length must be equal or greater than 8");
+              return;
+
+            }
 
 
 
